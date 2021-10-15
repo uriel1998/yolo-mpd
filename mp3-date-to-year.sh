6b20cbd7dc8b309010fa0e04aa7610ed3b616962
Steven Saus <steven@stevesaus.com>
2021-10-15 11:00:08 -0400

    Added self explanatory utility

---
+++ mp3-date-to-year.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+##############################################################################
+# This script is designed as a way to change the MP3 date to *just* a year
+##############################################################################
+startdir="$PWD"
+
+# find is not used here so that both operations can be done and so that
+# the whole operation doesn't die if mp3gain throws an error ungracefully
+
+IFS=$'\n'
+
+for f in $(find "$startdir" -name '*.mp3' );do 
+    rdate=$(eyeD3 -l critical "$f" | grep "release date" | awk -F ': ' '{ print $2 }' | awk -F '-' '{print $1}')
+    ordate=$(eyeD3 -l critical "$f" | grep "original release date" | awk -F ': ' '{ print $2 }' | awk -F '-' '{print $1}')
+    recdate=$(eyeD3 -l critical "$f" | grep "recording date" | awk -F ': ' '{ print $2 }' | awk -F '-' '{print $1}')
+
+	if [ -n "${rdate}" ];then
+        eyeD3 --quiet -l critical --release-date="${rdate}" "$f"
+    fi
+	if [ -n "${ordate}" ];then
+        eyeD3 --quiet -l critical --orig-release-date="${ordate}" "$f"
+    fi
+	if [ -n "${recdate}" ];then
+        eyeD3 --quiet -l critical --recording-date="${recdate}" "$f"
+    fi
+done
+
+unset IFS
