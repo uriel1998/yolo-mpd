3a151114a59d5185ef50cb0e48802e2a9d9da105
Steven Saus <steven@stevesaus.com>
2021-10-15 16:12:08 -0400

    Switch to ffprobe for speed, added TR to remove stray nulls and cr/lf

--- mp3-date-to-year.sh
+++ mp3-date-to-year.sh
@@ -10,19 +10,32 @@ startdir="$PWD"
 
 IFS=$'\n'
 
-for f in $(find "$startdir" -name '*.mp3' );do 
-    scratch=$(eyeD3 -l critical "$f")
-    o_rdate=$(echo "${scratch}" | grep -E "^release date:" | awk -F ': ' '{ print $2 }' )
-    o_ordate=$(echo "${scratch}" | grep -E "^original release date:" | awk -F ': ' '{ print $2 }' )
-    o_recdate=$(echo "${scratch}" | grep -E "^recording date:" | awk -F ': ' '{ print $2 }' )
 
-    rdate=$(echo "${scratch}" | grep -E "^release date:" | awk -F ': ' '{ print $2 }' | awk -F '-' '{print $1}')
-    ordate=$(echo "${scratch}" | grep -E "^original release date:" | awk -F ': ' '{ print $2 }' | awk -F '-' '{print $1}')
-    recdate=$(echo "${scratch}" | grep -E "^recording date:" | awk -F ': ' '{ print $2 }' | awk -F '-' '{print $1}')
+
+for f in $(find "$startdir" -name '*.mp3' );do 
+    
+    scratch=$(ffprobe "${f}" 2>&1 )
+    o_rdate=$(echo "${scratch}" | grep -E "^    date" | awk -F ': ' '{ print $2 }'  | tr -d [:cntrl:])
+    o_ordate=$(echo "${scratch}" | grep -E "^    originalyear" | awk -F ': ' '{ print $2 }'  | tr -d [:cntrl:])
+    o_recdate=$(echo "${scratch}" | grep -E "^    TDOR"| awk -F ': ' '{ print $2 }'  | tr -d [:cntrl:])
+
+    rdate=$(echo "${scratch}" | grep -E "^    date" | awk -F ': ' '{ print $2 }' | awk -F '-' '{print $1}' | tr -d [:cntrl:])
+    ordate=$(echo "${scratch}" | grep -E "^    originalyear" | awk -F ': ' '{ print $2 }' | awk -F '-' '{print $1}' | tr -d [:cntrl:])
+    recdate=$(echo "${scratch}" | grep -E "^    TDOR" | awk -F ': ' '{ print $2 }' | awk -F '-' '{print $1}' | tr -d [:cntrl:])
+    
+    # scratch=$(eyeD3 -l critical "${f}")
+    
+    # o_rdate=$(echo "${scratch}" | grep -E "^release date:" | awk -F ': ' '{ print $2 }' )
+    # o_ordate=$(echo "${scratch}" | grep -E "^original release date:" | awk -F ': ' '{ print $2 }' )
+    # o_recdate=$(echo "${scratch}" | grep -E "^recording date:" | awk -F ': ' '{ print $2 }' )
+
+    # rdate=$(echo "${scratch}" | grep -E "^release date:" | awk -F ': ' '{ print $2 }' | awk -F '-' '{print $1}')
+    # ordate=$(echo "${scratch}" | grep -E "^original release date:" | awk -F ': ' '{ print $2 }' | awk -F '-' '{print $1}')
+    # recdate=$(echo "${scratch}" | grep -E "^recording date:" | awk -F ': ' '{ print $2 }' | awk -F '-' '{print $1}')
 
 	if [ -n "${rdate}" ];then
         if [ "${o_rdate}" != "${rdate}" ];then
-            echo "### Changing release date for ${f}"
+            echo "### Changing release date for ${f} from ${o_rdate} ${rdate}"
             eyeD3 --quiet -l critical --release-date="${rdate}" "$f"
         else
             echo "No change needed with release date for ${f}"
@@ -38,7 +51,7 @@ for f in $(find "$startdir" -name '*.mp3' );do
     fi
 	if [ -n "${recdate}" ];then
         if [ "${o_recdate}" != "${recdate}" ];then
-            echo "### Changing recording date for ${f}"
+            echo "### Changing recording date for ${f} from ${o_recdate} to ${recdate}"
             eyeD3 --quiet -l critical --recording-date="${recdate}" "$f"
         else
             echo "No change needed with recording date for ${f}"
