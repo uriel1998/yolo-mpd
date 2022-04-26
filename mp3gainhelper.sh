d1e9097e31b2fdc4af9bde1cbd85f35d05b8f52d
Steven Saus <steven@stevesaus.com>
2022-04-26 16:20:08 -0400

    simplifying since the original problem seems to be gone...

--- mp3gainhelper.sh
+++ mp3gainhelper.sh
@@ -4,18 +4,20 @@
 # This script is designed as a wrapper for mp3gain that handles errors
 # gracefully, as well as automates the encoding of the replaygain info
 # into the id3 tags from APE
+# 
+# As the direct id3 tag writing seems to be working again, this is just
+# to simplify and speed up the process for me.
 ########################################################################
 
-
 if [ "$1" == "" ]; then
-	startdir="$PWD"
-	else
-		if [ -d "$1" ]; then
-			startdir="$1"
-		else
-			echo "Not a valid directory; exiting."
-			exit 1
-		fi
+    startdir="$PWD"
+else
+    if [ -d "$1" ]; then
+        startdir="$1"
+    else
+        echo "Not a valid directory; exiting."
+        exit 1
+    fi
 fi
 
 
@@ -25,13 +27,18 @@ fi
 IFS=$'\n'
 
 for f in $(find "$startdir" -name '*.mp3' );do 
-	mp3gain -c -a "$f"
-	mp3gain -c -r "$f"
+    # exists=$(ffprobe "${f}" 2>&1 | grep -c -e "replaygain_.*_gain")
+    # unneeded since direct writing to ID3v2 is working
+    mp3gain -e -c -T -p -r -k -s r -s i "${f}"
+    
+    # add -a and list of files for albumgain
+    # will need a whole separate pass for "album level" and then recurse into
+    # lower levels.
 done
 
 unset IFS
 
 #to ensure python2 compatibility for the moment
-source /home/steven/apps/ape2id3/bin/activate
-find . -type f -iname '*.mp3' -exec ape2id3.py -df {} \;
-deactivate
+#source /home/steven/apps/ape2id3/bin/activate
+#find . -type f -iname '*.mp3' -exec ape2id3.py -df {} \;
+#deactivate
