9b7f0efe4c32d2c396213af4a5894d9681c18d83
Steven Saus <steven@stevesaus.com>
2024-11-04 06:05:23 -0500

    A little toy

---
+++ show_music_video.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# I mean, it's just ... nevermind.  Anyway, next is to basically just do this for a playlist
+# and then you can (largely?) export your music playlists to youtube as well
+# or play them video-style locally or whatever.
+
+
+ytfzf -a -L  "$(cat ~/.cache/yadshow/songinfo) official video"
